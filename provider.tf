4b355b2c90715927f6ba1d7602f33768c4a191b4
Indu Karanam Ramaswamy Pillai <indhukaranam@gmail.com>
2024-04-10 08:48:09 +0000

    Initial Commit

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.2.0"
+    }
+  }
+backend "gcs" {
+    bucket  = "roidtc-240408-user523-tf-state"
+    prefix  = "terraform/state"
+  }
+}
+
+
+provider "google" {
+
+  project = var.project_id
+
+}
