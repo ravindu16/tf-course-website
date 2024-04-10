4dca94fc4a6d877894c228df92a92a35f72f2903
Indu Karanam Ramaswamy Pillai <indhukaranam@gmail.com>
2024-04-10 09:44:23 +0000

    ran terraform fmt

--- provider.tf
+++ provider.tf
@@ -5,9 +5,9 @@ terraform {
       version = "5.2.0"
     }
   }
-backend "gcs" {
-    bucket  = "roidtc-240408-user523-tf-state"
-    prefix  = "terraform/state"
+  backend "gcs" {
+    bucket = "roidtc-240408-user523-tf-state"
+    prefix = "terraform/state"
   }
 }
 
